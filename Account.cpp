627707a5a6347bfd7f571c6ba7e9d58657912545
anadeem717 <127272156+anadeem717@users.noreply.github.com>
2023-09-13 19:19:37 -0500

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -2,40 +2,54 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-using namespace std; // Use namespace std
+using namespace std; 
 
+// start assigning account numbers from 10000. Example, would be AN10001, AN10002, etc.
+int Account::assignAccountNum = 10000;
+
+// defining constructor for class Account
 Account::Account(const string& firstName, const string& lastName, const string& userName, const string& userPass, double balance)
-    : firstName(firstName), lastName(lastName), accountNum('A' + to_string(rand() % 10000 + 1000)), userName(userName), userPass(userPass), balance(balance) {}
 
+    :firstName(firstName), lastName(lastName), accountNum("AN" + to_string(assignAccountNum)), userName(userName), userPass(userPass), balance(balance) {
+
+    assignAccountNum++; // iterates to next account number
+}
+
+// function that initializes the user's username to their account 
 void Account::createUsername(const string& newUsername) {
     userName = newUsername;
 }
 
+// function that initializes the user's password to their account.
 void Account::createPass(const string& newPass) {
     userPass = newPass;
 }
 
+// checks to see if the attempted username and password matches the registered username / password
 bool Account::validateLogin(const string& attemptUsername, const string& attemptPass) {
-   
     return (attemptUsername == userName && attemptPass == userPass);
 }
 
+// method for depositing money into user's account
 void Account::deposit(double amount) {
     balance += amount;
-    cout << "$" << amount << " Successfully deposited. New balance is $" << balance << endl;
+    cout << "$" << amount << " Has been successfully deposited. New balance is $" << balance << endl;
 }
 
+// method for witdrawing money from user's account
 void Account::withdraw(double amount) {
     if (balance >= amount) {
         balance -= amount;
-        cout << "$" << amount << " Successfully withdrawn. New balance is $" << balance << endl;
-    } else {
-        cout << "Insufficient funds." << endl;
+        cout << "$" << amount << " Has been successfully withdrawn. New balance is $" << balance << endl;
+    } 
+    else {
+        cout << "Insufficient funds." << endl; // if the amount the user wants to withdraw exceeds their balance
     }
 }
 
+// method for displaying the user's account information
 void Account::display() const {
     cout << "Account information for " << firstName << " " << lastName << ":" << endl;
-    cout << "Account number: " << accountNum << endl;
-    cout << "Balance: $" << balance << endl;
+    cout << "Your Account number: " << accountNum << endl;
+    cout << "Total Balance: $" << balance << endl;
 }
